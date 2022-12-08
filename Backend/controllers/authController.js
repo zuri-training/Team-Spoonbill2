61b7bbd9d80b40e24ffe4dd608475c4c55f8499b
Ivan Okello <74784829+Ivandamien@users.noreply.github.com>
2022-12-08 16:35:47 +0300

    Implementing the user signup functionality

---
+++ Backend/controllers/authController.js
@@ -0,0 +1,24 @@
+const User = require("../models/userModel");
+
+exports.signup = async (req, res) => {
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm,
+    });
+
+    try {
+        res.status(201).json({
+            status: 'created',
+            data: {
+                newUser
+            }
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }
+}
