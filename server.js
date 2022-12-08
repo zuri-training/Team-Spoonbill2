776bee06ba1327a7f794d5857cfd697c91e5cf42
Ivan Okello <74784829+Ivandamien@users.noreply.github.com>
2022-12-08 12:34:32 +0300

    Implemented database connection succesful

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const mongoose = require('mongoose');
-
-
 const dotenv = require('dotenv');
 
-dotenv.config({ path: 'config.env' })
 
+
+
+dotenv.config({ path: 'config.env' });
 const app = require('./Backend/app');
 
-const port = process.env.PORT || 8000;
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+mongoose.connect(DB, {
+    useNewUrlParser:true,
+}).then(() => {
+    console.log("Database conection successfull");
+})
 
+
+
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
     console.log(`Server running at port ${port}`);
 });
