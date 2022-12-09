b419eb61575aac2b8b361b7f2bf1e0de1f034310
Ivan Okello <74784829+Ivandamien@users.noreply.github.com>
2022-12-09 15:13:31 +0300

    Setting up the next middle ware

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -57,7 +57,7 @@ userSchema.pre('save', async function (next) {
 
     // // Delete password field
     this.passwordConfirm = undefined
-    // next()
+    next()
 })
 
 const User = mongoose.model('User', userSchema);
