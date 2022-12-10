c289fcd530ebf0d73d5309e58ad71d951a4c0508
Ivan Okello <74784829+Ivandamien@users.noreply.github.com>
2022-12-10 16:20:27 +0300

    Completed the login functionality

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -4,6 +4,6 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/signup', authController.signup);
-
+router.post('/login', authController.login);
 
 module.exports=router
