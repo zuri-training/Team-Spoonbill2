a5e0b06e9648b1f5453ae946449bd8b7f942aaf7
Ivan Okello <74784829+Ivandamien@users.noreply.github.com>
2022-12-10 10:20:26 +0300

    Handling validation errors in db

--- Backend/controllers/errorController.js
+++ Backend/controllers/errorController.js
@@ -12,3 +12,8 @@ const handleDuplicatesFieldsDB = err => {
     const message = `Duplicate field value: ${value}.Please use another value`;
     return new AppError(message, 400);
 }
+const handleValidationErrorDB = err => {
+    const errors = Object.values(err.errors).map(el => el.message);
+    const message = `Invalid input data. ${errors.join('. ')}`;
+    return new AppError(message, 400);
+}
