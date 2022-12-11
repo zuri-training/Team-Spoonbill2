7fc411c56de211c47f594191ae41d529d894a0b0
Ivan Okello <74784829+Ivandamien@users.noreply.github.com>
2022-12-11 23:01:02 +0300

    Updated the usermodel for checking timestamp for password changes

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -81,6 +81,18 @@ userSchema.methods.correctPassword = async function (
     return await bcrypt.compare(candidatePassword, userPassword)
 };
 
+userSchema.methods.changePasswordAfter = function (JWTTimestamp) {
+    if (this.passwordChangedAt) {
+        const changedTimestamp = parseInt(
+            this.passwordChangedAt.getTime() / 1000, 10
+        );
+        return JWTTimestamp < changedTimestamp;
+    }
+
+    // False means NOT changed
+
+    return false
+}
 
 
 const User = mongoose.model('User', userSchema);
