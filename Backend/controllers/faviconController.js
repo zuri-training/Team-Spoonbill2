b43a7408e9d3a152f078c9c7d3d31569bc9a01d8
Ivan Okello <74784829+Ivandamien@users.noreply.github.com>
2022-12-12 23:49:27 +0300

    Instance method for password resetToken

--- Backend/controllers/faviconController.js
+++ Backend/controllers/faviconController.js
@@ -14,7 +14,7 @@ const storage = multer.diskStorage({
     },
     filename: (req, file, cb) => {
         console.log(file)
-        cb(null, Date.now() + path.extname(file.originalname));
+        cb(null,file.fieldname+ '_' + Date.now() + path.extname(file.originalname));
     }
 })
 
--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -94,6 +94,15 @@ userSchema.methods.changePasswordAfter = function (JWTTimestamp) {
     return false
 }
 
+userSchema.methods.createPasswordResetToken = function () {
+    const resetToken = crypto.randomBytes(32).toString('hex');
+    this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+    return resetToken;
+}
+
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
