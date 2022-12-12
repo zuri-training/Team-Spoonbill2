90117f3e74a3d938ceb9c2f2a73c731a04387a58
Ivan Okello <74784829+Ivandamien@users.noreply.github.com>
2022-12-12 07:25:48 +0300

    Defined favicon model

--- Backend/models/faviconModel.js
+++ Backend/models/faviconModel.js
@@ -1,1 +1,19 @@
 const mongoose = require('mongoose');
+
+const faviconSchema = new mongoose.Schema({
+    image: {
+        type: Buffer,
+        required:true
+    },
+    html: {
+        type: String,
+        required
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjctId,
+        ref:'User'
+    },
+});
+
+const Favicon = mongoose.model('Favicon', faviconSchema);
+module.exports = Favicon;
