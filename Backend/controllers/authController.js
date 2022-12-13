b2ab3be8ab86670db2cc10f95b32375c07f08239
Ivan Okello <74784829+Ivandamien@users.noreply.github.com>
2022-12-13 12:42:48 +0300

    Updated the reset password functionality

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -99,3 +99,31 @@ exports.forgotPassword = catchAsync(async(req, res, next) => {
     }
 
 })
+
+exports.resetPassword = catchAsync(async(req, res, next) => {
+    // 1) Get user based on the token
+    const hashedToken = crypto
+        .createHash('sha256')
+        .update(req.params.token)
+        .digest('hex');
+
+    const user = await User.findOne({
+        passwordResetToken: hashedToken,
+        passwordResetExpires: { $gt: Date.now() }
+    });
+
+    // 2)If token hasnt expired, and their is users, set the new password
+    if (!user) {
+        return next(new AppError('Token has expired or its invalid', 400));
+
+    }
+    user.password = req.body.password;
+    user.passwordConfirm = req.body.passwordConfirm;
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save();
+
+    // 3) Update changedPasswordAt property for the user
+    // 4) Log the user in, send JWT
+    createSendToken(user, 200, res)
+});
