7933c464dfbd567b93bb382c747fae371b71bc58
Ivan Okello <74784829+Ivandamien@users.noreply.github.com>
2022-12-13 12:34:58 +0300

    Forgot password functionality

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -64,3 +64,38 @@ exports.login = catchAsync(async (req, res, next) => {
 });
 
 
+exports.forgotPassword = catchAsync(async(req, res, next) => {
+    // 1) Get user based on posted email
+    const user = await User.findOne({ email: req.body.email })
+    if (!user) {
+        return next(new AppError('There is no user with that email address', 404))
+    }
+
+    // 2) Generate random reset token
+    const resetToken = user.createPasswordResetToken();
+    await user.save({ validateBeforeSave: false });
+
+    // 3) Send it to user's email
+    const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`;
+
+    const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email `;
+
+    try {
+        await sendEmail({
+            email: user.email,
+            subject: `Your password reset token (Valid for 10 min)`,
+            message
+        })
+        res.status(200).json({
+            status: 'success',
+            message: 'Token sent to email'
+        });
+    } catch (err) {
+        user.passwordResetToken = undefined;
+        user.passwordResetExpires = undefined;
+        await user.save({ validateBeforeSave: false });
+
+        return next(new AppError('There was an error sending the email. try again later!', 500))
+    }
+
+})
