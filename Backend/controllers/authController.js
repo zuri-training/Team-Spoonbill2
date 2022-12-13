2080bce697b8905e033649dfd910d09798f0ac7c
Ivan Okello <74784829+Ivandamien@users.noreply.github.com>
2022-12-13 13:40:31 +0300

    The logout middleware

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -67,6 +67,15 @@ exports.login = catchAsync(async (req, res, next) => {
 });
 
 
+exports.logout = (req, res) => {
+  res.cookie('jwt', 'loggedout', {
+      expires: new Date(Date.now() + 10 * 1000),
+      httpOnly: true
+  });
+  res.status(200).json({ status: 'success' });
+}
+
+
 exports.forgotPassword = catchAsync(async(req, res, next) => {
     // 1) Get user based on posted email
     const user = await User.findOne({ email: req.body.email })
